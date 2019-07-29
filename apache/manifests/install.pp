9dfbd662ef806eec801ef52a0bdef5b557760f9c
root <root@caleb831c.mylabserver.com>
2019-07-29 20:00:21 +0000

    Refactored module for Hiera

--- apache/manifests/install.pp
+++ apache/manifests/install.pp
@@ -2,13 +2,10 @@
 #
 # Installs the base Apache package.
 # @example
-class apache::install (
-  $install_name   = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
-) inherits apache::params {
-  package { "${install_name}":
-    ensure => $install_ensure,
+#
+class apache::install {
+  package { "${apache::install_name}":
+    ensure => $apache::install_ensure,
   }
 }
-
 
