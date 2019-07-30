54af3d665e68f27aaad03322d3f47a4ae8730314
root <root@caleb831c.mylabserver.com>
2019-07-30 13:29:01 +0000

    Added a service class

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -9,11 +9,19 @@ class apache  (
   String $install_ensure,
   String $config_ensure,
   String $config_path,
+  String $service_name,
+  Enum["running", "stopped"] $service_ensure,
+  Boolean $service_enable,
 ) {
   contain apache::install
   contain apache::config
+  contain apache::service
 
   Class['::apache::install']
   -> Class['::apache::config']
+  ~> Class['::apache::service']
 }
+
+
+
 
